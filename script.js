4b6fd7de9f09699f56985bad2f2174f239ed6f4c
jaydom20 <71834470+jaydom20@users.noreply.github.com>
2022-06-29 07:42:42 +0100

    Final Project draft

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const contact = document.getElementById("contact");
 button.addEventListener("click", function () {
   contact.scrollIntoView({ behavior: "smooth" });
 });
+
+// For sticky navbar
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
